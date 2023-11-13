43fa8a096a7b3225b14d74e558a59e22944c20e7
carla <carlajilo@gmail.com>
2023-11-13 18:55:36 +0100

    index.php

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -4,10 +4,21 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="usuario" class="form-label">Usuario</label>
+                <input type="text" class="form-control" id="user" name="user">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="pass">
+            </div>
+            <button type="submit" class="btn btn-primary" name="enviar">Entrar</button>
+        </form>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
